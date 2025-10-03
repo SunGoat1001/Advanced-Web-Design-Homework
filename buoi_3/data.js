450627838385003a19d47b832767b2f03d464de9
SunGoat1001 <ngoctien10012004@gmail.com>
2025-10-03 16:33:42 +0700

    update exercises for sessions 1, 2, 3,

---
+++ buoi_3/data.js
@@ -0,0 +1,33 @@
+const womenClothes = [
+    {
+        id: 'KO0002',
+        name: 'Quần jean nữ',
+        image: 'https://cdn.vuahanghieu.com/unsafe/0x900/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2023/06/quan-jean-nu-weird-market-tight-bell-bottom-jeans-faded-denim-mau-xanh-nhat-647ffcabb8e03-07062023104235.jpg',
+        oldPrice: 370000,
+        newPrice: 269000
+    },
+    {
+        id: 'VN0002',
+        name: 'Áo khoác nữ',
+        image: 'https://product.hstatic.net/200000313079/product/upload_dab8bfb73b9048b2a86760e10f907110_master.jpg',
+        oldPrice: 369000,
+        newPrice: 249000
+    }
+];
+
+const menClothes = [
+    {
+        id: 'KI0002',
+        name: 'Quần jean nam',
+        image: 'https://pos.nvncdn.com/2865a9-85186/ps/20240716_GgH0HuDypf.jpeg?v=1721095949',
+        oldPrice: 370000,
+        newPrice: 269000
+    },
+    {
+        id: 'VM0002',
+        name: 'Áo khoác nam',
+        image: 'https://product.hstatic.net/1000369857/product/akd903_1_tui_1200x1200_0002_layer_21_c8306b98e3604f5890c8446b99cf2a9b.jpg',
+        oldPrice: 369000,
+        newPrice: 249000
+    }
+];
